58a030081d62c2f89bb08cf1bd0ca35f5680af98
mjqhrgx <mjqhrgx@163.com>
2025-12-24 13:07:19 +0800

    Initial commit

---
+++ src/test/java/org/sqx/javaaidemo/JavaAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sqx.javaaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiDemoApplicationTests {
+	
+	@Test
+	void contextLoads () {
+	}
+	
+}
